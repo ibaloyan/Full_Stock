22e7c69bf824de9c39b76d5e546f888f809f4158
mjr390 <mjr117@yahoo.com>
2018-09-16 14:35:02 -0400

    created a json to use to plot bitcoin vs itself for half the year, this data works on a route in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_pymongo import PyMongo
 # init the Flask
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = 'mongodb://localhost:27017/nhl_db'
+app.config["MONGO_URI"] = 'mongodb://localhost:27017/bitcoin_y_o_y'
 mongo = PyMongo(app)
 
 # Pass connection to the pymongo instance.
@@ -15,41 +15,42 @@ mongo = PyMongo(app)
 db = mongo.db
 
 # create a route for scraping
-@app.route("/apple")
+@app.route("/bit_y_o_y")
 ##### Give new name
 def Apple():
-    appl = db.articles.find_one()
-    print(appl)
-    appl_title = appl["title"]
-    appl_dict = {"titel":appl_title}
-    print(appl_title)
+    btcY = db.bit_y_o_y.find_one()
+    print(btcY)
+    btcY_title = btcY["labels"]
+    btcY_numbers = btcY["numbers"]
+    btcY_dict = {"title":btcY_title, "numbers": btcY_numbers}
+    print(btcY_title)
     # return appl_title
-    return jsonify(appl_dict)
-    
+    return jsonify(btcY_dict)
+
 
 # @app.route("/amazon")
 # ##### Give new name
 # def AMZA():
 #     amaz = list(db.collection_name.find())
-#     return jsonify(amaz)   
+#     return jsonify(amaz)
 
 # @app.route("/microsoft")
 # ##### Give new name
 # def micro():
 #     msft = list(db.collection_name.find())
-#     return jsonify(msft) 
+#     return jsonify(msft)
 
 # @app.route("/google")
 # ##### Give new name
 # def Goog():
 #     goog = list(db.collection_name.find())
-#     return jsonify(goog)  
+#     return jsonify(goog)
 
 # @app.route("/bitcoin")
 # ##### Give new name
 # def BTC():
 #     btc = list(db.collection_name.find())
-#     return jsonify(btc)          
+#     return jsonify(btc)
 
 # create a index route
 @app.route('/')
