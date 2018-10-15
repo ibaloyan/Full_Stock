e3fa4e65b1892aaec8cb6a7d6d32ea7f6301dc71
ibaloyan <ibaloyan@gmail.com>
2018-10-15 03:04:42 -0400

    Changed DatesN to Dates in plot1.js

--- static/js/plot1.js
+++ static/js/plot1.js
@@ -24,9 +24,9 @@ function buildPlot3(stock3) {
     var name3 = "IBM"; // for the legend on the side of the graph
     var labels3 = data3.title;
     var startDate = "1962-01-31";
-    var endDate = "2018-09-30";
-    // var dates3 = data3.Dates;
-    var dates3 = data3.DatesN;
+    var endDate = "2018-10-31";
+    var dates3 = data3.Dates;
+    // var dates3 = data3.DatesN;
     var PercentChanges3 = data3.Change;
     console.log( "Data3", data3)
 
@@ -108,8 +108,8 @@ function buildPlot1(stock1) {
     var labels1 = data1.title;
     // var startDate = "1980-12-31";
     // var endDate = "2018-09-30";
-    // var dates1 = data1.Dates;
-    var dates1 = data1.DatesN;
+    var dates1 = data1.Dates;
+    // var dates1 = data1.DatesN;
     var PercentChanges1 = data1.Change;
     console.log( "Data1", data1)
 
@@ -162,8 +162,8 @@ function buildPlot2(stock2) {
     var labels2 = data2.title;
     // var startDate = "1980-12-31";
     // var endDate = "2018-09-30";
-    // var dates2 = data2.Dates;
-    var dates2 = data2.DatesN;
+    var dates2 = data2.Dates;
+    // var dates2 = data2.DatesN;
     var PercentChanges2 = data2.Change;
     console.log( "Data2", data2)
 
@@ -216,8 +216,8 @@ function buildPlot4(stock4) {
     var labels4 = data4.title;
     // var startDate = "1980-12-31";
     // var endDate = "2018-09-30";
-    // var dates4 = data4.Dates;
-    var dates4 = data4.DatesN;
+    var dates4 = data4.Dates;
+    // var dates4 = data4.DatesN;
     var PercentChanges4 = data4.Change;
     // for( var i = 0 ; i < PercentChanges4.length ; i++ ) {
     //   PercentChanges4[i] = PercentChanges4[i] + "%";
@@ -248,10 +248,6 @@ function buildPlot4(stock4) {
 
       // Update Plot1 with Intel Data
        Plotly.addTraces("plot1", my_data4);
-
-       // add two traces
-// Plotly.addTraces(graphDiv, [{y: [2,1,2]}, {y: [4, 5, 7]}]);
-// Plotly.addTraces(graphDiv, {y: [2,1,2]});
 
   });
 }
